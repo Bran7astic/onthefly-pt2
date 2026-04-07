96826b8ba8a4df0d850caab62b53ae9f49fa67b6
Brandon Budhan <bran7astic@ool-18bd48f3.dyn.optonline.net>
2026-04-07 15:30:11 -0400

    step 4 complete

--- client/src/pages/CreateDestination.jsx
+++ client/src/pages/CreateDestination.jsx
@@ -24,17 +24,42 @@ const CreateDestination = () => {
 
         const addDestination = async () => {
 
- 
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(destination)
+            }
         
+            const response = await fetch(`/api/destinations`, options)
+            const data = await response.json()
+
+            setDestination(data)
+            return data.id
+
         }
 
         const createTripDestination = async (destination_id) => {
 
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    trip_id: trip_id,
+                    destination_id: destination_id
+                })
+            }
 
+            const response = await fetch(`/api/trips-destinations`, options)
+            const data = await response.json()
+            return data
         
         }
 
-
+        addDestination().then(res => createTripDestination(res)).then(res => window.location = '/')
     }
 
     return (
